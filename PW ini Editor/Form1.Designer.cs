708e8c6fefcc7dfd70c6319c420d9fbcbfdc5b29
Admin <Admin@DESKTOP-GORJCHL>
2022-05-15 18:01:28 +0800

    Add project files.

---
+++ PW ini Editor/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace PW_ini_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(326, 22);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(66, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Sharpen";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(173, 22);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(70, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Shadows";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 22);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(77, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Windowed";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(12, 45);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(60, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "V-Sync";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(173, 43);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(77, 17);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Sun/Moon";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(326, 45);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(52, 17);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "Warp";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "640x480",
+            "800x600",
+            "1024x768",
+            "1280x720",
+            "1280x768",
+            "1280x800",
+            "1280x960",
+            "1280x1024",
+            "1600x900",
+            "1600x1024",
+            "1680x1050",
+            "1768x992",
+            "1920x1080"});
+            this.comboBox1.Location = new System.Drawing.Point(173, 64);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.Text = "Select Resolution";
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(12, 68);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(77, 17);
+            this.checkBox7.TabIndex = 10;
+            this.checkBox7.Text = "16:9 Mode";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.CheckedChanged += new System.EventHandler(this.checkBox7_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 116);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PW Settings Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox7;
+    }
+}
+
---
+++ PW ini Editor/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using Salaros.Configuration;
+
+namespace PW_ini_Editor
+{
+
+    public partial class Form1 : Form
+
+    {
+
+        private string appConfig = Application.StartupPath + @"\element\userdata\systemsettings.ini";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            var cfg = new ConfigParser(appConfig);
+
+            // Window Mode
+            if (this.checkBox3.Checked)
+            {
+                this.TopMost = true;
+                this.checkBox3.Text = "Window Mode Enabled";
+                this.checkBox3.Checked = true;
+                cfg.SetValue("Video", "FullScreen", "0");
+                cfg.Save();
+            }
+            else
+            {
+                this.TopMost = false;
+                this.checkBox3.Text = "Window Mode Disabled";
+                this.checkBox3.Checked = false;
+                cfg.SetValue("Video", "FullScreen", "1");
+                cfg.Save();
+            }
+
+
+        }
+
+        void LoadSettings()
+        {
+            var cfg = new ConfigParser(appConfig);
+            var fssetting = cfg.GetValue("Video", "FullScreen");
+            var shadows = cfg.GetValue("Video", "Shadow");
+            var vsync = cfg.GetValue("Video", "VerticalSync");
+            var warp = cfg.GetValue("Video", "SpaceWarp");
+            var sunmoon = cfg.GetValue("Video", "SunFlare");
+            var widescreen = cfg.GetValue("Video", "WideScreen");
+
+            if (shadows == "0")
+            {
+                this.TopMost = true;
+                this.checkBox2.Text = "Shadows Enabled";
+                this.checkBox2.Checked = true;
+            }
+            else
+            {
+                this.TopMost = false;
+                this.checkBox2.Text = "Shadows Disabled";
+                this.checkBox2.Checked = false;
+                cfg.SetValue("Video", "Shadow", "1");
+            }
+
+            if (fssetting == "0")
+            {
+                this.TopMost = true;
+                this.checkBox3.Text = "Window Mode Enabled";
+                this.checkBox3.Checked = true;
+            }
+            else
+            {
+                this.TopMost = false;
+                this.checkBox3.Text = "Window Mode Disabled";
+                this.checkBox3.Checked = false;
+                cfg.SetValue("Video", "FullScreen", "1");
+            }
+
+            if (vsync == "0")
+            {
+                this.TopMost = true;
+                this.checkBox4.Text = "V-Sync Enabled";
+                this.checkBox4.Checked = true;
+            }
+            else
+            {
+                this.TopMost = false;
+                this.checkBox4.Text = "V-Sync Disabled";
+                this.checkBox4.Checked = false;
+                cfg.SetValue("Video", "FullScreen", "1");
+            }
+
+            if (widescreen == "0")
+            {
+                this.TopMost = true;
+                this.checkBox7.Text = "16:9 Mode Enabled";
+                this.checkBox7.Checked = true;
+            }
+            else
+            {
+                this.TopMost = false;
+                this.checkBox7.Text = "16:9 Mode Disabled";
+                this.checkBox7.Checked = false;
+                cfg.SetValue("Video", "WideScreen", "1");
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadSettings();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            var cfg = new ConfigParser(appConfig);
+
+            // Shadows
+            if (this.checkBox2.Checked)
+            {
+                this.TopMost = true;
+                this.checkBox2.Text = "Shadows Enabled";
+                this.checkBox2.Checked = true;
+                cfg.SetValue("Video", "Shadow", "0");
+                cfg.Save();
+            }
+            else
+            {
+                this.TopMost = false;
+                this.checkBox2.Text = "Shadows Disabled";
+                this.checkBox2.Checked = false;
+                cfg.SetValue("Video", "Shadow", "1");
+                cfg.Save();
+            }
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            var cfg = new ConfigParser(appConfig);
+
+            // Shadows
+            if (this.checkBox4.Checked)
+            {
+                this.TopMost = true;
+                this.checkBox4.Text = "V-sync Enabled";
+                this.checkBox4.Checked = true;
+                cfg.SetValue("Video", "VerticalSync", "0");
+                cfg.Save();
+            }
+            else
+            {
+                this.TopMost = false;
+                this.checkBox4.Text = "V-sync Disabled";
+                this.checkBox4.Checked = false;
+                cfg.SetValue("Video", "VerticalSync", "1");
+                cfg.Save();
+            }
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+            var cfg = new ConfigParser(appConfig);
+
+            // Widescreen Mode
+            if (this.checkBox7.Checked)
+            {
+                this.TopMost = true;
+                this.checkBox7.Text = "16:9 Mode Enabled";
+                this.checkBox7.Checked = true;
+                cfg.SetValue("Video", "WideScreen", "0");
+                cfg.Save();
+            }
+            else
+            {
+                this.TopMost = false;
+                this.checkBox7.Text = "16:9 Mode Disabled";
+                this.checkBox7.Checked = false;
+                cfg.SetValue("Video", "WideScreen", "1");
+
+            }
+        }
+    }
+
+}
